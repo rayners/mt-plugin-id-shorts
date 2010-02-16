136e3040033ddb5a1d704500db6424f1174e2792
David Raynes <rayners@gmail.com>
2010-02-16 01:35:02 -0500

    Added first pass at optional click tracking

--- plugins/IdShorts/lib/IdShorts/App.pm
+++ plugins/IdShorts/lib/IdShorts/App.pm
@@ -21,11 +21,30 @@ sub redirect_mode {
     
     require MT::Entry;
     my $e = MT::Entry->load ($id) or return $app->error ('Unknown entry');
-    
+    $app->{__entry} = $e;
+
     require MT::Util;
     my $link = MT::Util::strip_index ($e->permalink, $e->blog);
     return $app->redirect ($link);
 }
 
+sub takedown {
+    my $app = shift;
+    my $e = $app->{__entry};
+    if ($e) {
+	# link was successful
+	# record it if they want
+	my $plugin = MT->component('idshorts');
+	if ($plugin->get_config_value ('track_clicks', 'blog:' . $e->blog_id)) {
+	    # blog has tracking turned on
+	    # so let's track it
+	    my $clicks = $e->id_shorts_clicks || 0;
+	    $e->id_shorts_clicks ($clicks + 1);
+	    $e->meta_obj->save;
+	}
+    }
+
+    $app->SUPER::takedown();
+}
 
 1;
