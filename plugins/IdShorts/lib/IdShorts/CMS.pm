10f4eb2fe88537c31ca7dd4926190966321efa28
David Raynes <rayners@gmail.com>
2010-03-04 22:53:51 -0500

    Added edit entry source callback

---
+++ plugins/IdShorts/lib/IdShorts/CMS.pm
@@ -0,0 +1,22 @@
+
+package IdShorts::CMS;
+
+use strict;
+use warnings;
+
+sub edit_entry_source {
+    my ( $cb, $app, $tmpl ) = @_;
+
+    $$tmpl
+        =~ s/(\Q<li class="pings-link">\E.*?<\/li>)/$1<li><__trans phrase="[quant,_1,shorts click,shorts clicks]" params="<$mt:var name="id_shorts_clicks"$>"<\/li>/;
+
+    return 1;
+}
+
+sub edit_entry_param {
+    my ( $cb, $app, $params, $tmpl ) = @_;
+
+    return 1;
+}
+
+1;
