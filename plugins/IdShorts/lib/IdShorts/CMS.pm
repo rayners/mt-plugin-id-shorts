9efd0a6daf128f967bfddeeeb8f691e6472bcc65
David Raynes <rayners@gmail.com>
2010-04-09 23:50:09 -0400

    Added short url to edit entry page.

--- plugins/IdShorts/lib/IdShorts/CMS.pm
+++ plugins/IdShorts/lib/IdShorts/CMS.pm
@@ -8,7 +8,7 @@ sub edit_entry_source {
     my ( $cb, $app, $tmpl ) = @_;
 
     $$tmpl
-        =~ s/(\Q<li class="pings-link">\E.*?<\/li>)/$1<li class="short_clicks"><__trans phrase="[quant,_1,shorts click,shorts clicks]" params="<\$mt:var name="id_shorts_clicks"\$>"><\/li>/;
+        =~ s/(\Q<li class="pings-link">\E.*?<\/li>)/$1<li class="short_clicks"><__trans phrase="[quant,_1,shorts click,shorts clicks]" params="<\$mt:var name="id_shorts_clicks"\$>"> on <a href="<\$mt:var name="id_shorts_url"\$>"><\$mt:var name="id_shorts_url"\$><\/a><\/li>/;
 
     return 1;
 }
@@ -20,6 +20,9 @@ sub cms_edit_entry {
     return 1 if ( !$id );
 
     $params->{id_shorts_clicks} = $obj->id_shorts_clicks;
+
+    require IdShorts::Util;
+    $params->{id_shorts_url} = IdShorts::Util->short_url_for($obj);
 
     return 1;
 }
